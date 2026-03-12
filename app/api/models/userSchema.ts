82487aaab72d9d1bb642eb9c6f2cd44198302f32
Chu A. <chu.amk22@gmail.com>
2026-03-12 12:14:14 +0100

    feat: add user schema model for managing user data in the application

---
+++ app/api/models/userSchema.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firebaseUid: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  displayName: { type: String, required: true },
+  photoURL: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
