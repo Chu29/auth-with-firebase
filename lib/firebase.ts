7a6efb5a136f96456782bad46c0e01fdfbc82fe7
Chu A. <chu.amk22@gmail.com>
2026-03-12 02:20:23 +0100

    add firebase initialization and authentication providers

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth/web-extension";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const githubProvider = new GithubAuthProvider();
